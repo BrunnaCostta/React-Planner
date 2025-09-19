c9ffaef2ded26ba2794145d5dc72a39a82e4ea7a
Bruna Costa <brunadacosta21@yahoo.com.br>
2025-09-19 19:38:30 -0300

    feat-creating backend

---
+++ backend/server.js
@@ -0,0 +1,67 @@
+import express from "express";
+
+import { PrismaClient } from "../src/generated/prisma/index.js";
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+app.post("/usuario", async (req, resp) => {
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+  resp.status(201).json(req.body);
+});
+
+// 1 - tipo de rota/método http
+// 2 - endereço
+
+app.listen(5173);
+
+app.get("/usuario", async (req, resp) => {
+  let users = [];
+
+  if (req.query.name) {
+    users = await prisma.user.findMany({
+      where: {
+        name: req.query.name,
+        email: req.query.email,
+        age: req.query.age,
+      },
+    });
+  } else {
+    users = await prisma.user.findMany();
+  }
+
+  resp.status(200).json(users);
+});
+
+app.put("/usuario/:id", async (req, resp) => {
+  await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+  console.log(req);
+  resp.status(201).json(req.body);
+});
+
+app.delete("/usuario/:id", async (req, resp) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  resp.status(200).json({ message: "Usuário deletado com suceso" });
+});
